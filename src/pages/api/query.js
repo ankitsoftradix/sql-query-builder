d73c3f18fb58f5eedb9e9ad24d12933217a39158
Satvinder Singh <satvinder@softradix.in>
2024-01-11 19:44:49 +0530

    query builder apis done

--- src/pages/api/query.js
+++ src/pages/api/query.js
@@ -2,6 +2,16 @@ import nextConnect from 'next-connect';
 import middleware from '../../../middleware/database'; // Import the database middleware
 import pool from '../../../config/db'; // Import the database connection
 import bodyParser from 'body-parser';
+import Cors from 'cors'; // Import the cors middleware
+
+import initMiddleware from '../../../lib/init-middleware'; // Create a separate file to initialize middleware
+// Initialize the Cors middleware
+const cors = initMiddleware(
+  Cors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'POST'], // Add the HTTP methods you want to allow
+  })
+);
 
 // Function to handle POST requests
 async function handler(req, res) {
@@ -32,7 +42,6 @@ async function handler(req, res) {
       if(req.body.aggregation == "COUNT" || req.body.aggregation == "SUM" || req.body.aggregation == "AVG"){
         query = `SELECT ${req.body.aggregation}(${req.body.field}) FROM ${req.body.table}`;
       }
-  
     }
     if(req.body.is_join==1) {
       req.body.is_where = 0;
@@ -40,42 +49,42 @@ async function handler(req, res) {
    
     if(req.body.is_where == 1) {
   
-      if(!req.body.compare_field) {
-        res.status(400).json({
-          status: 0, 
-          code:400, 
-          message: 'Compare field is required!', 
-          data: null 
-        });
-      }
+      // if(!req.body.compare_field) {
+      //   res.status(400).json({
+      //     status: 0, 
+      //     code:400, 
+      //     message: 'Compare field is required!', 
+      //     data: null 
+      //   });
+      // }
   
-      if(!req.body.operator) {
-        res.status(400).json({
-          status: 0, 
-          code:400, 
-          message: 'Operator is required!', 
-          data: null 
-        });
-      }
+      // if(!req.body.operator) {
+      //   res.status(400).json({
+      //     status: 0, 
+      //     code:400, 
+      //     message: 'Operator is required!', 
+      //     data: null 
+      //   });
+      // }
   
-      if(!req.body.value) {
-        res.status(400).json({
-          status: 0, 
-          code:400, 
-          message: 'Value is required!', 
-          data: null 
-        });
-      }
-      query += ` where ${req.body.compare_field} ${req.body.operator} '${req.body.value}'`
-    }
+      // if(!req.body.value) {
+      //   res.status(400).json({
+      //     status: 0, 
+      //     code:400, 
+      //     message: 'Value is required!', 
+      //     data: null 
+      //   });
+      // }
+      // query += ` where ${req.body.compare_field} ${req.body.operator} '${req.body.value}'`
   
-    if(req.body.is_join !=1) {
-      if(req.body.is_condition.length > 0) {
+      if(req.body.is_condition && req.body.is_condition.length > 0) {
         for(let i in req.body.is_condition) {
-          query += ` ${req.body.is_condition[i].condition_type} ${req.body.is_condition[i].condition_field} = '${req.body.is_condition[i].condition_value}'`
+          query += ` where ${req.body.is_condition[i].condition_type ? req.body.is_condition[i].condition_type: ""} ${req.body.is_condition[i].condition_field} ${req.body.is_condition[i].operator} '${req.body.is_condition[i].condition_value}'`
         }
       }
     }
+
+    console.log(query, "====query==")
   
     if(req.body.is_join==1) {
   
@@ -139,5 +148,11 @@ async function handler(req, res) {
   }
 
 }
+
+// handler.use(cors({
+//   origin: '*', // Specify the allowed origin
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+// }));
 
 export default handler;
